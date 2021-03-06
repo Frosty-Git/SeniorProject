be1bda2f035e9c068ada1ed05814157b2c4fc08e
elliottt1 <elliottt1@students.rowan.edu>
2021-03-06 12:36:26 -0500

    Models created for song and playliist posts

--- django/social_feed/models.py
+++ django/social_feed/models.py
@@ -8,10 +8,16 @@ class Post(models.Model):
     date_last_updated = models.DateTimeField(auto_now_add=True)
 
 # SongPost
-
+class SongPost(Post):
+    song = models.ForeignKey(Musicdata)
+    def __str__(self):
+        return self.song
 
 # PlaylistPost
+class PlaylistPost(Post):
+    playlist = models.ForeignKey(Playlist)
 
-
+    def __str__(self):
+        return self.playlist
 # Comment
 
