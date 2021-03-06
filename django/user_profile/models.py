ecd66bd8e669b68ce656b224fc4a8e9ee0ca48e4
Jacelynn Duranceau <duranceaj3@students.rowan.edu>
2021-03-06 11:59:46 -0500

    Added Settings Model

--- django/user_profile/models.py
+++ django/user_profile/models.py
@@ -4,7 +4,17 @@ from django.db import models
 
 
 # Settings
+class Settings(models.Model):
+    user_id = models.ForeignKey(UserProfile)
+    private_profile = models.BooleanField(default=False)
+    private_playlists = models.BooleanField(default=False)
+    light_mode = models.BooleanField(default=False)
+    explicit_music = models.BooleanField(default=False)
+    live_music = models.BooleanField(default=False)
+    objects = models.Manager()
 
+    def __str__(self):
+        return "Setting"
 
 # Preferences
 
