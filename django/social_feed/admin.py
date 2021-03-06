e6ac79764be35cecec1cec34ffd2da4946237dc4
Frosty <joeyfrost007@gmail.com>
2021-03-06 13:33:43 -0500

    Added models to admin files.

--- django/social_feed/admin.py
+++ django/social_feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+from .models import Comment
+from .models import SongPost
+from .models import PlaylistPost
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(SongPost)
+admin.site.register(PlaylistPost)
--- django/user_profile/admin.py
+++ django/user_profile/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
+from .models import Settings
+from .models import Preferences
+from .models import Playlist
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Settings)
+admin.site.register(Preferences)
+admin.site.register(Playlist)
