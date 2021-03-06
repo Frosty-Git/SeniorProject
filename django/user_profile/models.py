19544875710baf58d7882894c9f6fe83c753b539
Jacelynn Duranceau <duranceaj3@students.rowan.edu>
2021-03-06 12:35:12 -0500

    PLaylist model added

--- django/user_profile/models.py
+++ django/user_profile/models.py
@@ -20,4 +20,16 @@ class Settings(models.Model):
 
 
 # Playlist
-
+    user_profile_fk = models.ForeignKey(UserProfile) # Who created the playlist
+    music_data_fk = models.ForeignKey(MusicData, null=True) # Songs in the playlist
+    name = models.CharField(max_length=30)
+    image = models.ImageField(null=True) # Pillow, add upload_to attribute
+    upvotes = models.IntegerField(default=0) 
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_last_updated = models.DateTimeField(auto_now_add=True)
+    theme = models.TextField()  # Genres
+    # this_weeks_upvotes = models.IntegerField()
+    # length = # Derived
+    # num_songs = # Derived
+    # num_followers = # Derived
+    objects = models.Manager()
