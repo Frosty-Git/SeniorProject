77d7f235e043e1e9f6fca74c2748918b31a2086d
katiejlee <ktlee6455@gmail.com>
2021-03-06 12:23:44 -0500

    Merge branch 'DB_Models' of https://github.com/Frosty-Git/SeniorProject into DB_Models

--- django/user_profile/models.py
+++ django/user_profile/models.py
@@ -26,7 +26,17 @@ class UserProfile(models.Model):
         return self.user.username
 
 # Settings
+class Settings(models.Model):
+    user_id = models.ForeignKey(UserProfile)
+    private_profile = models.BooleanField(default=False)
+    private_playlists = models.BooleanField(default=False)
+    light_mode = models.BooleanField(default=False)
+    explicit_music = models.BooleanField(default=False)
+    live_music = models.BooleanField(default=False)
+    objects = models.Manager()
 
+    def __str__(self):
+        return "Setting"
 
 # Preferences
 
