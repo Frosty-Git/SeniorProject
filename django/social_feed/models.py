cf8fc6cb89df26e2cefb407a2ea65e8916f7be77
Frosty <joeyfrost007@gmail.com>
2021-03-06 13:18:34 -0500

    Recreated my comments and user profile foreign key in Comment model.

--- django/social_feed/models.py
+++ django/social_feed/models.py
@@ -4,6 +4,12 @@ from user_profile.models import *
 
 # Post
 class Post(models.Model):
+    """
+    Author: Joseph Frost
+    Created: 2020.03.06
+    Creates a Post model for the db. The post is a post on the social
+    feed created by the user. 
+    """
     date_created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     date_last_updated = models.DateTimeField(auto_now_add=True)
@@ -35,11 +41,18 @@ class PlaylistPost(Post):
         return self.playlist
 # Comment
 class Comment(models.Model):
+    """
+    Author: Joseph Frost
+    Created: 2020.03.06
+    Creates a Comment model for the db. The comment is a comment on
+    a post on the social feed.
+    """
     date_created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     date_last_updated = models.DateTimeField(auto_now_add=True)
     upvotes = models.IntegerField(default=0)
     post_fk = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_profile_fk = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
