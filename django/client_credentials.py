371512159ce64b7f5905cc1200fb0634cbe1b23c
elliottt1 <elliottt1@students.rowan.edu>
2021-03-09 16:05:10 -0500

    Adding files related to spotipy authorization.

---
+++ django/client_credentials.py
@@ -0,0 +1,4 @@
+import os
+os.environ['SPOTIPY_CLIENT_ID'] = '3a9428a4172b47889e14f1f80f2ddec2'
+os.environ['SPOTIPY_CLIENT_SECRET'] = '129eacf7acab436c9df791e9e42b5465'
+os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8000/recommender/home/'
---
+++ django/recommender/client_auth.py
@@ -0,0 +1,25 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+
+scope = "playlist-modify-public"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+#results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+print(sp.me())
+username = sp.me()['id']
+print(username)
+#sp.user_playlist_create(username, 'test', public=True, collaborative=False, description="This is a test")
+playlist = sp.current_user_playlists(limit=1)
+
+print(playlist)
+playlist_items = playlist['items']
+print(playlist_items)
+playlist_id = playlist_items[0]
+print(playlist_id['id'])
+
+sp.user_playlist_add_tracks(username, playlist_id['id'], tracks=['spotify:track:65OVbaJR5O1RmwOQx0875b'], position=1)
