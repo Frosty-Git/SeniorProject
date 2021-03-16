d8f3c1c7bc03776724796ab7b435f911469b8381
katiejlee <ktlee6455@gmail.com>
2021-03-16 17:56:04 -0400

    added password change to account settings

--- django/user_profile/urls.py
+++ django/user_profile/urls.py
@@ -14,9 +14,14 @@ urlpatterns = [
     path('following/<user_id>', views.display_following, name='following'),
     path('unfollow/<user_id>/<who>', views.unfollow, name='unfollow'),
     path('update_profile/', views.update_profile, name='update_profile'),
+
+    # Password related URLs
     path('account_setting/', auth_views.PasswordChangeView.as_view(
             template_name='settings/account_setting.html',
-            success_url = '/user/account_setting/'), name='account_setting'),
+            success_url='/user/password_change_done/'), name='account_setting'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(
+            template_name='settings/password_change_done.html',), name='password_change_done'),
+    
     # path('userprofile/<user_id>', views.other_profile, name='other_profile'),
     # path('num_followers/<user_id>', views.num_followers, name='num_followers')
 ]
