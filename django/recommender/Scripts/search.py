22b41bd51095efe48906c39d8e429ff593000ad2
Frosty <joeyfrost007@gmail.com>
2021-03-23 13:36:46 -0400

    Created search_audio_features function for the survey crew.

--- django/recommender/Scripts/search.py
+++ django/recommender/Scripts/search.py
@@ -9,6 +9,11 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 #RESULTS_RETURNED = 5
 
 def search_tracks(query, RESULTS_RETURNED, offset):
+    """
+    Searches Spotify for tracks that match the query.
+    Returns the number of results specified in RESULTS_RETURNED.
+    The offset changes the starting index of the returned results.
+    """
     track_ids=[]
     result = sp.search(q=query, limit=RESULTS_RETURNED, offset=offset, type='track', market=None)
     for x in range(RESULTS_RETURNED):
@@ -18,6 +23,11 @@ def search_tracks(query, RESULTS_RETURNED, offset):
     return track_ids
 
 def search_albums(query, RESULTS_RETURNED, offset):
+    """
+    Searches Spotify for albums that match the query.
+    Returns the number of results specified in RESULTS_RETURNED.
+    The offset changes the starting index of the returned results.
+    """
     album_ids=[]
     result = sp.search(q=query, limit=RESULTS_RETURNED, offset=offset, type='album', market=None)
     for y in range(RESULTS_RETURNED):
@@ -27,6 +37,11 @@ def search_albums(query, RESULTS_RETURNED, offset):
     return album_ids
 
 def search_artists(query, RESULTS_RETURNED, offset):
+    """
+    Searches Spotify for artists that match the query.
+    Returns the number of results specified in RESULTS_RETURNED.
+    The offset changes the starting index of the returned results.
+    """
     artist_ids=[]
     result = sp.search(q=query, limit=RESULTS_RETURNED, offset=offset, type='artist', market=None)
     for z in range(RESULTS_RETURNED):
@@ -34,3 +49,13 @@ def search_artists(query, RESULTS_RETURNED, offset):
             break
         artist_ids.append(result['artists']['items'][z]['id'])
     return artist_ids
+
+def search_audio_features(query):
+    """
+    Enter in a song name. Returns the audio features of that song.
+    Make sure to be very specific with your query to get the correct
+    song. These are the same audio features for the Kaggle data.
+    """
+    track = search_tracks(query, 1, 0)
+    features = sp.audio_features(tracks=[track])
+    return features
