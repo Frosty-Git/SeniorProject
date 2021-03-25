565149579b94624ff0d137b5b139ba38bfcd2365
Joke65 <gamerdudeiii@yahoo.com>
2021-03-25 16:44:19 -0400

    Created a new form and updated the survey page, however it is still not displaying the widgets.

--- django/recommender/forms.py
+++ django/recommender/forms.py
@@ -13,3 +13,7 @@ class ArtistForm(forms.Form):
 
 class SongForm(forms.Form):
     song_title = forms.CharField(label='Artist name', max_length=100)
+
+class SurveyForm(forms.Form):
+    artist_name = forms.CharField(label='Artist name', max_length=100)
+    test_hidden_field = forms.CharField(label='Hidden Field', max_length=100)
