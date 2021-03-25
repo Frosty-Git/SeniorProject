b09b1eeb4890c7634632293e60f4627e600bda02
Frosty <joeyfrost007@gmail.com>
2021-03-25 19:08:44 -0400

    Fixed indents in spotify_token.py.

--- django/recommender/Scripts/spotify_token.py
+++ django/recommender/Scripts/spotify_token.py
@@ -3,10 +3,10 @@
 def save_token(request, authenticator):
     user_id = request.user.id
     session = request.session.get('_auth_user_id')
-        if int(user_id) == int(session):
-            #check for token expiration
-            request.session['_sp_auth_token'] = authenticator.get_cached_token()
-            sptoken = request.session['_sp_auth_token']
-        else:
-            request.session['_sp_auth_token'] = None
-            sptoken = request.session['_sp_auth_token']
+    if int(user_id) == int(session):
+        #check for token expiration
+        request.session['_sp_auth_token'] = authenticator.get_cached_token()
+        sptoken = request.session['_sp_auth_token']
+    else:
+        request.session['_sp_auth_token'] = None
+        sptoken = request.session['_sp_auth_token']
