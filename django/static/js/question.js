04af3552e253d6dfcf234bbaf5a4976aa41146cc
Joke65 <gamerdudeiii@yahoo.com>
2021-03-31 14:11:56 -0400

    Created a single question for song preference. Testing purposes. More Q's to come.

--- django/static/js/question.js
+++ django/static/js/question.js
@@ -139,7 +139,24 @@ let questions = [
       [null, null, null, null, null],
       //valence
       [null, null, null, null, null],),
-    new Question("Which of the following songs do you listen to the most?", [""])
+    new Question("Which of the following songs do you listen to the most?", ["In My Life", "Yesterday", "Twist and Shout", "Come Together", "Strawberry Fields Forever"],
+    //danceability
+    [0.688, 0.332, 0.482, 0.533, 0.39],
+    //acousticness
+    [0.449, 0.879, 0.641, 0.0302, 0.336],
+    //energy
+    [0.435, 0.179, 0.849, 0.376, 0.502],
+    //instrumentalness
+    [0, 0, 0.00000774, 0.248, 0.000138],
+    //speechiness
+    [0.0323, 0.0326, 0.0452, 0.0393, 0.178],
+    //loudness
+    [-11.359, -11.83, -9.198, -11.913, -12.277],
+    //tempo
+    [103.239, 96.529, 124.631, 165.007, 97.871],
+    //valence
+    [0.435, 0.315, 0.937, 0.187, 0.289],
+    ),
 
 
 ];
