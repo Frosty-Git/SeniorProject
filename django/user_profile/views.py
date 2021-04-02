9e1e6b2e3cdee7c4b38b2c8d43c27fc764ddc0c6
katiejlee <ktlee6455@gmail.com>
2021-04-02 13:11:17 -0400

    idk

--- django/user_profile/views.py
+++ django/user_profile/views.py
@@ -660,7 +660,9 @@ def add_song_to_playlist(request, location):
     if request.method == 'POST':
         # user_id = request.user.id
         # user = UserProfile.objects.get(pk=user_id)
+        print('is this working')
         track_term = request.POST.get('track_id')
+        print('track_term')
         song = SongId.objects.get(pk=track_term)
         playlist_id = request.POST.get('playlist_id')
         playlist = Playlist.objects.get(pk=playlist_id)
