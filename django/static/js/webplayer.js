1c36f5c6bfe4b368c00a1f0bc068fa0ed784c5ee
katiejlee <ktlee6455@gmail.com>
2021-04-19 21:21:41 -0400

    got seeking to work on volume and song

--- django/static/js/webplayer.js
+++ django/static/js/webplayer.js
@@ -157,11 +157,39 @@ window.onSpotifyWebPlaybackSDKReady = () => {
         });
     });
 
-    $('#volume').mouseUp(function() {
+    $('#volume').mouseup(function() {
         let volume_value = $(this).val();
         player.setVolume(volume_value/100);
         console.log(`The volume of the player is ${volume_value}%`);
     });
+
+    $('#songprogress').mouseup(function() {
+        let seek_value = $(this).val();
+        player.seek(seek_value);
+        console.log(`The player is now at ${seek_value} seconds`);
+    });
+
+    $('#volumeBtn').click(function() {
+        player.getVolume().then(volume => {
+            if (volume == 0) {
+                player.setVolume(0.5);
+                $('#volume').val(0.5 * 100);
+                $(this).html('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-volume-down-fill" viewBox="0 0 16 16">'
+                            + '<path d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12V4zm3.025 4a4.486 4.486 0 0 1-1.318 3.182L10 10.475A3.489 3.489 0 0 0 11.025 8 3.49 3.49 0 0 0 10 5.525l.707-.707A4.486 4.486 0 0 1 12.025 8z"/>'
+                            + '</svg>')
+                console.log('Sound on');
+            }
+            else {
+                player.setVolume(0);
+                $('#volume').val(0);
+                $(this).html('<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-volume-mute-fill" viewBox="0 0 16 16">'
+                            + '<path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>'
+                            + '</svg>')
+                console.log('Muted');
+            }
+        })
+    });
+
 
 };
 
