50f78044a595cc53e2db5ce7581205368a5509b3
jacelynnduranceau <duranceaj3@students.rowan.edu>
2021-04-26 09:50:59 -0400

    started working on code cleanup but gotta merge branches first oops

--- django/recommender/forms.py
+++ django/recommender/forms.py
@@ -1,9 +1,4 @@
 from django import forms
-
-class SearchForm(forms.Form):
-    artist = forms.CharField(widget=forms.TextInput(attrs={'size': '50'}))
-    from_year = forms.IntegerField(required=False)
-    to_year = forms.IntegerField(required=False)
     
 class OurSearchForm(forms.Form):
     term = forms.CharField(widget=forms.TextInput(attrs={'size':'50'}))
--- django/recommender/urls.py
+++ django/recommender/urls.py
@@ -5,11 +5,8 @@ app_name = 'recommender'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('best/', views.searchform_get, name='best'),
-    path('bestp/', views.searchform_post, name='bestp'),
     path('results/', views.results, name='results'),
     path('about/', views.about, name='about'),
-    path('survey/', views.survey, name='survey'),
     path('top_tracks/', views.top_tracks, name='top_tracks'),
     path('artist/', views.searchArtist_get, name='artist'),
     path('artistp/', views.searchArtist_post, name='artistp'),
